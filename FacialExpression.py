3e6578d0fa1d09d05f9db8f1e72536d200018668
Atsuro <situniv.a26@gmail.com>
2023-12-14 13:44:15 +0900

    fix : get landmark (in the middle)

---
+++ FacialExpression.py
@@ -0,0 +1,18 @@
+LEFT_EYEBROW_POINT	= []
+RIGHT_EYEBROW_POINT = []
+LEFT_EYE_POINT		= []
+RIGHT_EYE_POINT		= []
+MOUTH_POINT			= []
+
+class FacialExpression():
+	def detect_expression(self, landmark):
+		#–Ú‚Ì‘å‚«‚³
+		
+		
+		#ŒûŠp
+		
+		
+		#”ûŠÔŠu
+		
+		
+		#”ûŠp“x

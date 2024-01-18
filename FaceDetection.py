6fa242f90d611e589fafa24450b82f1e7e445442
Atsuro <situniv.a26@gmail.com>
2024-01-18 10:49:03 +0900

    fix

--- FaceDetection.py
+++ FaceDetection.py
@@ -18,6 +18,7 @@ class FaceDetection():
 
         drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
         cap = cv2.VideoCapture(0)
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
         with mp_face_mesh.FaceMesh(
             max_num_faces=1,
             refine_landmarks=True,
@@ -40,6 +41,14 @@ class FaceDetection():
                         detectoin = facial.detect_expression(landmarks, width, height)
                         # cv2.circle(image, (right_corner[0],right_corner[1]), 5, (255,255,0))
                 
+                # cv2.putText(image, 
+                #                 text='3', 
+                #                 org=(100,300), 
+                #                 fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                #                 fontScale=10.0,
+                #                 color=(0, 255, 0),
+                #                 thickness=5,
+                #                 lineType=cv2.LINE_4)
                 cv2.imshow('MediaPipe Face Mesh', cv2.flip(image, 1))
                 if cv2.waitKey(5) & 0xFF == 27:
                     break
@@ -50,19 +59,28 @@ class FaceDetection():
                     count = 0
                 
                 pre_deteciton = detectoin
-                if count < 30:
+                if count < fps:
+                    
                     continue
 
                 if detectoin == 1:
                     print("smile")
+                    exp = cv2.imread("./Resource/smile.png")
                 elif detectoin == 2:
                     print("angry")
+                    exp = cv2.imread("./Resource/angry.png")
                 elif detectoin == 3:
                     print("sad")
+                    exp = cv2.imread("./Resource/sad.png")
                 elif detectoin == 4:
                     print("suprised")
+                    exp = cv2.imread("./Resource/surprised.png")
                 elif detectoin == 5:
                     print("normal")
+                    exp = cv2.imread("./Resource/normal.png")
+                cv2.imshow('Expression', exp)
+                
+                
         cap.release()
 
 if __name__ == "__main__":
